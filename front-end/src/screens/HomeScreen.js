75f5abf9d2c956f653053d8ad18bb042eebf698f
alferno <aniandp@gmail.com>
2020-12-07 21:10:36 +0530

    frontend and backend running together

--- front-end/src/screens/HomeScreen.js
+++ front-end/src/screens/HomeScreen.js
@@ -1,9 +1,19 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { Row, Col } from 'react-bootstrap'
 import Product from '../components/Product'
-import products from '../products'
+import Axios from 'axios'
 
 const HomeScreen = () => {
+    const [products, setProducts] = useState([])
+
+    useEffect(() => {
+        const fetchProducts = async () => {
+            const { data } = await Axios.get('/api/products')
+            
+            setProducts(data)
+        }
+        fetchProducts()
+    }, [])
     return (
         <>
         <h1>Latest Products</h1>
