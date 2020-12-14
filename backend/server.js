4cc71afac20622c1f47e43133f1077f934ac011e
alferno <aniandp@gmail.com>
2020-12-14 21:38:22 +0530

    auth and token

--- backend/server.js
+++ backend/server.js
@@ -2,8 +2,9 @@ import express from 'express'
 import connectDB  from './config/db.js'
 import colors from 'colors'
 import dotenv from 'dotenv'
-import productRoutes from './routes/productRoutes.js'
 import { notFound, errorHandler } from './middleware/errorMiddleware.js'
+import productRoutes from './routes/productRoutes.js'
+import userRoutes from './routes/userRoutes.js'
 
 
 dotenv.config()
@@ -12,11 +13,15 @@ connectDB()
 
 const app = express()
 
+app.use(express.json())
+
 app.get('/', (req, res) => {
     res.send('API is runnig...')
 })
 
+
 app.use('/api/products',productRoutes)
+app.use('/api/users',userRoutes)
 
 app.use(notFound)
 app.use(errorHandler)
